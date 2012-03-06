10d64dbcb7e2a63f5578838f900677274a9af89b
PPYMOU <ppymou@gmail.com>
2012-03-06 10:48:24 -0500

    H

--- src/Safetrack/tracker/supportFunc.py
+++ src/Safetrack/tracker/supportFunc.py
@@ -1,3 +1,33 @@
+from Safetrack.tracker.models import SafetyConstraint
+
 #Support Functions
 def checkStatus(modelObj):
     return {'safety':"Safe",'temp':'12C','humid':'??','noise':'20Db','impact':'0G'}
+
+def getLatestData(user):
+    safetyConstraints = SafetyConstraint.objects.all()
+    latestDataItem = SensorData.objects.filter(user=user).order_by('-time')[0]
+    latestDataItems = SensorData.objects.filter(time=latestDataItem.time)
+    temp = latestDataItems.filter(sensorType='T')[0].value
+    noise = latestDataItems.filter(sensorType='N')[0].value
+    humidity = latestDataItems.filter(sensorType='H')[0].value
+    impact = latestDataItems.filter(sensorType='I')[0].value
+    # get latest data
+    
+    isSafe = True
+    dangerValues = [];
+    for constraint in safetyConstraints:
+        for dataItem in latestDataItems:
+            if dataItem.sensorType == constraint.sensorType:
+                if dataItem.value > constraint.maxValue or dataItem.value < constraint.minValue:
+                    isSafe = False
+                    isHigh = False;
+                    sensorName = ""
+                    if dataItem.value > constraint.maxValue:
+                        isHigh = True;
+                    if constraint.sensorType == 'T' : sensorName = "Temperature"
+                    if constraint.sensorType == 'N' : sensorName = "Noise"
+                    if constraint.sensorType == 'I' : sensorName = "Impact"
+                    if constraint.sensorType == 'H' : sensorName = "Humidity"
+                    dangerValues.append({"dataItem":dataItem,"constraint":constraint,"isHigh":isHigh,"sensorName":sensorName})
+    return [isSafe, dangerValues, {'temp':temp,'humid':humidity,'noise':noise,'impact':impact}]
--- src/Safetrack/tracker/views.py
+++ src/Safetrack/tracker/views.py
@@ -80,34 +80,6 @@ def loginView(request):
 
     return HttpResponse(t.render(c))
 
-def getLatestData(user):
-    safetyConstraints = SafetyConstraint.objects.all()
-    latestDataItem = SensorData.objects.filter(user=user).order_by('-time')[0]
-    latestDataItems = SensorData.objects.filter(time=latestDataItem.time)
-    temp = latestDataItems.filter(sensorType='T')[0].value
-    noise = latestDataItems.filter(sensorType='N')[0].value
-    humidity = latestDataItems.filter(sensorType='H')[0].value
-    impact = latestDataItems.filter(sensorType='I')[0].value
-    # get latest data
-    
-    isSafe = True
-    dangerValues = [];
-    for constraint in safetyConstraints:
-        for dataItem in latestDataItems:
-            if dataItem.sensorType == constraint.sensorType:
-                if dataItem.value > constraint.maxValue or dataItem.value < constraint.minValue:
-                    isSafe = False
-                    isHigh = False;
-                    sensorName = ""
-                    if dataItem.value > constraint.maxValue:
-                        isHigh = True;
-                    if constraint.sensorType == 'T' : sensorName = "Temperature"
-                    if constraint.sensorType == 'N' : sensorName = "Noise"
-                    if constraint.sensorType == 'I' : sensorName = "Impact"
-                    if constraint.sensorType == 'H' : sensorName = "Humidity"
-                    dangerValues.append({"dataItem":dataItem,"constraint":constraint,"isHigh":isHigh,"sensorName":sensorName})
-    return [isSafe, dangerValues, {'temp':temp,'humid':humidity,'noise':noise,'impact':impact}]
-
 def renderDataEmployee(request):
     if not authorized(request):
         return loginView(request)
