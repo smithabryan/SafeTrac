8e48ad545f15aac9306d62055609ac7e5ff8d01a
root <root@linux.(none)>
2012-03-06 00:24:06 +0000

    So far

--- src/Safetrack/tracker/static/scripts/dragdrop.js
+++ src/Safetrack/tracker/static/scripts/dragdrop.js
@@ -1,9 +1,17 @@
+var monitored = new Object()
+var usersStatusURL = "ajax/usersStatus.py" //returns data in JSON
+var usersGraphURL = "????"
+
+$.ajaxSetup ({
+    cache: false
+});
+
 $(function() {
     var viewBlk = $('#viewBlk');
     var groupBlk = $('#groupBlk');
     
     $('li',$('#groupBlk')).draggable();
-    
+   
     groupBlk.droppable({
         accept: "#group li",
         activeClass: "highlightSelection",
@@ -18,3 +26,24 @@ $(function() {
         }
     });
 });
+
+function latestInfo(users) {
+    $.getJSON(
+        usersStatusURL,
+        {users:monitored},
+
+        function(data) {
+            $.each(data,function(key,val) {
+                $("#"+key).find('stat').html(val)
+                if (val == "Danger") {
+                    //add styles !!!
+                }
+            })
+        });
+    //graph AJAX call - Zach?
+    $.getJSON()
+
+}
+
+//background ajax calls every 30seconds
+periodicCheck = setInterval(latestInfo(user),30000)
